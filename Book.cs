3df8351f5d99a41ecf43cffa1ab92486e74c8a4a
Markulak-Dunja <68125364+Markulak-Dunja@users.noreply.github.com>
2020-07-13 16:57:57 +0200

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Books
+{
+    public class Person
+    {
+        public string name, lastname, gender;
+        public Person(string firstn, string lastn, string gen)
+        {
+            name = firstn;
+            lastname = lastn;
+            gender = gen;
+        }
+    }
+    public abstract class Book
+    {
+        public string name { get; }
+        protected string ISBN;
+        public Person author { get; }
+        public string publisher, variation;
+        public int edition, pageNum, numRates;
+        public double rate;
+
+
+        public Book(string name1, Person author1, string publisher1, int edition1, int pageNum1, string ISBN1)
+        {
+            name = name1;
+            author = author1;
+            publisher = publisher1;
+            edition = edition1;
+            pageNum = pageNum1;
+
+            if (ISBN1.Length == 13)
+                ISBN = ISBN1;
+            else
+                throw new System.ArgumentException("invalid ISBN");
+
+            if (variation != "paperback" || variation != "hardcover")
+                ISBN = ISBN1;
+            else
+                throw new System.ArgumentException("invalid atribute variation");
+
+            rate = 0;
+            numRates = 0;
+        }
+        public abstract void Info();
+
+        public double GetRate()
+        {
+            if (numRates > 0)
+            {
+                return rate / numRates;
+
+            }
+
+            else
+                Console.WriteLine("The book hasn't been rated yet");
+            return -1;
+
+        }
+
+    }
+}
---
+++ Novel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Books
+{
+    public class Novel:Book
+    {
+        public static List<string> all_genre = new List<string> { "Crime", "Drama", "Fantasy", "Adventure", "SF", "Fairytale", "Mystery", "Poetry", "Romance", "Tragedy" };
+
+        public string genre;
+        public bool status;
+        private int specId { set; get; }
+
+        public Novel(string name1, Person author1, string genre1, string publisher1, int edition1, int pageNum1, string ISBN1) :
+            base(name1, author1, publisher1, edition1, pageNum1, ISBN1)
+        {
+            if (all_genre.Contains(genre1))
+                genre = genre1;
+            else
+                throw new System.ArgumentException("genre is unknown");
+        }
+
+        public override void Info()
+        {
+            Console.WriteLine("name: {0} \n author: {1} \n publisher: {2} \n edition: {3}. edition \n genre: {4}",
+                name, author, publisher, edition, genre);
+            return;
+        }
+
+    }
+}
