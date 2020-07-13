9a16d705ce5349e2b9f396e74e66fe8610b4e66f
Markulak-Dunja <68125364+Markulak-Dunja@users.noreply.github.com>
2020-07-13 18:03:28 +0200

    Create Person.cs

---
+++ day1/Person.cs
@@ -0,0 +1,18 @@
+using System;
+
+
+namespace Books
+{
+    public class Person
+    {
+
+        public string name, lastname, gender;
+        public Person(string firstn, string lastn, string gen)
+        {
+            name = firstn;
+            lastname = lastn;
+            gender = gen;
+          
+        }
+    }
+}
